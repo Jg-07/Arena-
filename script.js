8c277bc92ce5e6668ebf30c97000f00f8f350aca
joshuakye <38572206+joshuakye@users.noreply.github.com>
2021-04-03 21:35:16 -0700

    Added the sidebar and search bar with styling.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const humb1 = document.querySelector('.HmBurger');
 const boxer1 =  document.querySelector('.box-container');
 const cbox = document.querySelector('.CrossBurger');
+const imageTitle = document.querySelector('.title');
+const img = document.querySelector('.Display');
 humb1.addEventListener("click",function() {
    boxer1.classList.remove('hidden');
    humb1.classList.add('hidden');
@@ -11,3 +13,23 @@ cbox.addEventListener('click',function(){
    humb1.classList.remove('hidden');
    cbox.classList.add('hidden');
 });
+img.addEventListener('mouseenter',function(){
+   img.classList.remove('opac2');
+   img.classList.add('opac1');
+   imageTitle.classList.remove('hidden');
+})
+img.addEventListener('mouseout',function(){
+   imageTitle.classList.add('hidden');
+   img.classList.remove('opac1');
+   img.classList.add('opac2');
+})
+imageTitle.addEventListener('mouseenter',function(){
+   img.classList.remove('opac2');
+   img.classList.add('opac1');
+   imageTitle.classList.remove('hidden');
+})
+imageTitle.addEventListener('mouseout',function(){
+   imageTitle.classList.add('hidden');
+   img.classList.remove('opac1');
+   img.classList.add('opac2');
+})
