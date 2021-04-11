35d00837d5ab05ab331c932c1373a9fb2ed9d77e
jaias <jaiastrobaskar@gmail.com>
2021-04-11 22:32:27 +0530

    Changes in Sidebar navigation

--- script.js
+++ script.js
@@ -5,10 +5,16 @@ const imageTitle = document.querySelector('.title');
 const img = document.querySelector('.Display');
 const basicBooms = document.querySelector('#BB');
 const basicAgg = document.querySelector('#BA');
+const advBooms =document.querySelector('#AB');
+const advAgg = document.querySelector('#AA');
+const civSpecific = document.querySelector('#CS');
 const mainbody =document.querySelector('.bodyBlock1');
 const home = document.querySelector('#home');
 const bboombody = document.querySelector('.bodyBlock2');
 const baggbody =document.querySelector('.bodyBlock3');
+const aboombody = document.querySelector('.bodyBlock4');
+const aaggbody = document.querySelector('.bodyBlock5');
+const civsbody = document.querySelector('.bodyBlock6');
 humb1.addEventListener("click",function() {
    boxer1.classList.remove('hidden');
    humb1.classList.add('hidden');
@@ -42,15 +48,48 @@ imageTitle.addEventListener('mouseout',function(){
 basicBooms.addEventListener("click", function(){
   mainbody.classList.add('hidden');
   baggbody.classList.add('hidden');
+  aboombody.classList.add('hidden');
+  aaggbody.classList.add('hidden');
+  civsbody.classList.add('hidden');
   bboombody.classList.remove('hidden');
-})
-home.addEventListener('click',function(){
-   bboombody.classList.add('hidden');
-   baggbody.classList.add('hidden');
-   mainbody.classList.remove('hidden');
 })
 basicAgg.addEventListener('click',function(){
    mainbody.classList.add('hidden');
    bboombody.classList.add('hidden');
+   aboombody.classList.add('hidden');
+   aaggbody.classList.add('hidden');
+   civsbody.classList.add('hidden');
    baggbody.classList.remove('hidden');
+})
+advBooms.addEventListener("click", function(){
+   mainbody.classList.add('hidden');
+   baggbody.classList.add('hidden');
+   bboombody.classList.add('hidden');
+   aaggbody.classList.add('hidden');
+   civsbody.classList.add('hidden');
+   aboombody.classList.remove('hidden');
+ })
+advAgg.addEventListener("click", function(){
+   mainbody.classList.add('hidden');
+   baggbody.classList.add('hidden');
+   bboombody.classList.add('hidden');
+   aboombody.classList.add('hidden');
+   civsbody.classList.add('hidden');
+   aaggbody.classList.remove('hidden');
+ })
+civSpecific.addEventListener("click", function(){
+   mainbody.classList.add('hidden');
+   baggbody.classList.add('hidden');
+   bboombody.classList.add('hidden');
+   aaggbody.classList.add('hidden');
+   aboombody.classList.add('hidden');
+   civsbody.classList.remove('hidden');
+ })
+home.addEventListener('click',function(){
+   bboombody.classList.add('hidden');
+   baggbody.classList.add('hidden');
+   aaggbody.classList.add('hidden');
+   aboombody.classList.add('hidden');
+   civsbody.classList.add('hidden');
+   mainbody.classList.remove('hidden');
 })
